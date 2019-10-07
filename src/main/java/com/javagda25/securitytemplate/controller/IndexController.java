ee5d4caaa999707303bc6b25b0079b9e0e15d1f4
Paweł Recław <pawel.reclaw@gmail.com>
2019-10-07 19:36:41 +0200

    Login form

--- src/main/java/com/javagda25/securitytemplate/controller/IndexController.java
+++ src/main/java/com/javagda25/securitytemplate/controller/IndexController.java
@@ -1,15 +1,24 @@
 package com.javagda25.securitytemplate.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = "/")
+@AllArgsConstructor
 public class IndexController {
 
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String loginForm(){
+        return "login-form";
     }
 }
