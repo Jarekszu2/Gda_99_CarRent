dcbf5343f84f4bcb8a2b4253b754bddcb81109d3
Paweł Recław <pawel.reclaw@gmail.com>
2019-10-07 18:18:46 +0200

    Init commit

---
+++ src/main/java/com/javagda25/securitytemplate/SecuritytemplateApplication.java
@@ -0,0 +1,13 @@
+package com.javagda25.securitytemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritytemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritytemplateApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/javagda25/securitytemplate/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.javagda25.securitytemplate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/")
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
